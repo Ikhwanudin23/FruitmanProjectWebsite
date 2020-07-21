cb13b75f907a34cc3c8e3a9c9b85ec773fcc89f2
ikhwanudin23 <ikhwanudin2309@gmail.com>
2020-07-21 14:26:49 +0700

    fixx

--- resources/views/auth-admin/loginForm.blade.php
+++ resources/views/auth-admin/loginForm.blade.php
@@ -5,33 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="{{asset('Assets/AssetsLoginForm/images/icons/favicon.ico')}}"/>
+    <link rel="icon" type="image/png" href="{{asset('assets/AssetsLoginForm/images/icons/favicon.ico')}}"/>
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('Assets/AssetsLoginForm/vendor/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/AssetsLoginForm/vendor/bootstrap/css/bootstrap.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('Assets/AssetsLoginForm/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/AssetsLoginForm/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('Assets/AssetsLoginForm/fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/AssetsLoginForm/fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('Assets/AssetsLoginForm/vendor/animate/animate.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/AssetsLoginForm/vendor/animate/animate.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('Assets/AssetsLoginForm/vendor/css-hamburgers/hamburgers.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/AssetsLoginForm/vendor/css-hamburgers/hamburgers.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('Assets/AssetsLoginForm/vendor/select2/select2.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/AssetsLoginForm/vendor/select2/select2.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{asset('Assets/AssetsLoginForm/css/util.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('Assets/AssetsLoginForm/css/main.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/AssetsLoginForm/css/util.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/AssetsLoginForm/css/main.css')}}">
     <!--===============================================================================================-->
 </head>
 <body>
 
 <div class="limiter">
-    <div class="container-login100" style="background-image: url('{{asset('Assets/AssetsLoginForm/images/img-01.jpg')}}');">
+    <div class="container-login100" style="background-image: url('{{asset('assets/AssetsLoginForm/images/img-01.jpg')}}');">
         <div class="wrap-login100 p-t-190 p-b-30">
             <form class="login100-form validate-form" method="POST" action="{{ route('admin.login.submit') }}">
                 @csrf
                 <div class="login100-form-avatar">
-                    <img src="{{asset('Assets/AssetsLoginForm/images/avatar-01.jpg')}}" alt="AVATAR">
+                    <img src="{{asset('assets/AssetsLoginForm/images/avatar-01.jpg')}}" alt="AVATAR">
                 </div>
 
                 <span class="login100-form-title p-t-20 p-b-45">
@@ -83,14 +83,14 @@
 
 
 <!--===============================================================================================-->
-<script src="{{asset('Assets/AssetsLoginForm/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
+<script src="{{asset('assets/AssetsLoginForm/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
 <!--===============================================================================================-->
-<script src="{{asset('Assets/AssetsLoginForm/vendor/bootstrap/js/popper.js')}}"></script>
-<script src="{{asset('Assets/AssetsLoginForm/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/AssetsLoginForm/vendor/bootstrap/js/popper.js')}}"></script>
+<script src="{{asset('assets/AssetsLoginForm/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
 <!--===============================================================================================-->
-<script src="{{asset('Assets/AssetsLoginForm/vendor/select2/select2.min.js')}}"></script>
+<script src="{{asset('assets/AssetsLoginForm/vendor/select2/select2.min.js')}}"></script>
 <!--===============================================================================================-->
-<script src="{{asset('Assets/AssetsLoginForm/js/main.js')}}"></script>
+<script src="{{asset('assets/AssetsLoginForm/js/main.js')}}"></script>
 
 </body>
 </html>
